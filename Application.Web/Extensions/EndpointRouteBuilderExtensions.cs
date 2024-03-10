65c2881c37cf4adea1c9e60aaccd5e735bba602d
Francesco Paolocci <micky5991@gmail.com>
2024-03-10 11:49:03 +0100

    Add extensions to allow easy redirects

---
+++ Application.Web/Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Application.Web.Extensions;
+
+// https://khalidabuhakmeh.com/simple-redirects-with-aspnet-core-endpoint-routing
+public static class EndpointRouteBuilderExtensions
+{
+    public static IEndpointRouteBuilder Redirect(this IEndpointRouteBuilder endpoints, string from, string to)
+    {
+        return Redirect(endpoints, new Redirective(from, to));
+    }
+
+    public static IEndpointRouteBuilder RedirectPermanent(this IEndpointRouteBuilder endpoints, string from, string to)
+    {
+        return Redirect(endpoints, new Redirective(from, to, true));
+    }
+
+    public static IEndpointRouteBuilder Redirect(this IEndpointRouteBuilder endpoints, params Redirective[] paths)
+    {
+        foreach (var (from, to, permanent) in paths)
+        {
+            endpoints.MapGet(from, http =>
+            {
+                http.Response.Redirect(to, permanent);
+
+                return Task.CompletedTask;
+            });
+        }
+
+        return endpoints;
+    }
+
+    public record Redirective(string From, string To, bool Permanent = false);
+}
