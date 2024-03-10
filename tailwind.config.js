473a47857ce567cd7e95d3491cae0367ee2fed13
Francesco Paolocci <micky5991@gmail.com>
2024-03-10 21:18:08 +0100

    Finish translating simple login pages to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./**/*.{razor,cshtml,html}'],
     theme: {
+        extend: {
+            colors: {
+                primary: colors.blue,
+            }
+        }
     },
     plugins: [
         require("@tailwindcss/typography"),
